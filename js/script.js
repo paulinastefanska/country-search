bd7587c9bbe75ac0a9963e474a1edcfe4653ca6e
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-21 14:07:42 +0200

    Add js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = document.getElementById('countries');
+
+document.getElementById('search').addEventListener('click', searchCountries);
+
+function searchCountries() {
+    var countryName = document.getElementById('country-name').value;
+    if(!countryName.length) countryName = 'Poland';
+    fetch(url + countryName)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(showCountriesList);
+}
+
+function showCountriesList(resp) {
+  countriesList.innerHTML = '';
+}
