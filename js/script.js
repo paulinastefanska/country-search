e9911194dc71f476d6898f953e2ee2cd00be1b68
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-21 14:11:29 +0200

    Add function to show countries

--- js/script.js
+++ js/script.js
@@ -17,4 +17,9 @@ function searchCountries() {
 
 function showCountriesList(resp) {
   countriesList.innerHTML = '';
+  resp.forEach(function(item){
+    var liEl = document.createElement('li');
+    liEl.innerText = item.name;
+    countriesList.appendChild(liEl);
+  });
 }
