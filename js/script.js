5a2b6cec446631f0d7b941272b9073cd28f9f4aa
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-23 14:05:22 +0200

    Add styles and enter click button

--- js/script.js
+++ js/script.js
@@ -3,8 +3,17 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = document.getElementById('countries');
 
+// Event listener for search button
 document.getElementById('search').addEventListener('click', searchCountries);
 
+// Enter button click
+document.querySelector("#country-name").addEventListener("keyup", function(e) {
+  if (e.keyCode == 13) {
+    searchCountries();
+  }
+});
+
+// Function fetch countries data
 function searchCountries() {
     var countryName = document.getElementById('country-name').value;
     if(!countryName.length) countryName = 'Poland';
@@ -15,6 +24,7 @@ function searchCountries() {
         .then(showCountriesList);
 }
 
+// Clear country card list and generating new card (from template) for each country found
 function showCountriesList(resp) {
 	countriesList.innerHTML = '';
 	resp.forEach(function(country){
